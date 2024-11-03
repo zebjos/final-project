5f45535a87838871c5d14a14268901a393e26f5e
zebjos <zebjosefsson@gmail.com>
2024-11-03 21:07:09 +0100

    Added border and shape to GameItem Card

--- app/src/main/java/com/example/final_project/components/GameItem.kt
+++ app/src/main/java/com/example/final_project/components/GameItem.kt
@@ -1,13 +1,16 @@
 package com.example.final_project.components
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.final_project.data.GameModel
 
@@ -17,7 +20,9 @@ fun GameItem(game: GameModel, onJoinClick: () -> Unit) {
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp)
+            .border(1.dp, Color.Blue, shape = RoundedCornerShape(8.dp))
             .clickable { onJoinClick() },
+        shape = RoundedCornerShape(8.dp)
     ) {
         Column(modifier = Modifier.padding(16.dp)) {
             Text("Game ID: ${game.gameId}")
